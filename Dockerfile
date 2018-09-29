8465fa6ab3bdc7af4c45c74141c09d64a1019ff1
pitabas prathal <pitabas.prathal@gmail.com>
2018-09-29 21:01:53 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM ehazlett/tomcat7
+COPY simple-web-app-master.war /opt/tomcat/webapps/simple-web-app-master.war
+
---
+++ init.sql
@@ -0,0 +1,2 @@
+create table user_account (id int not null auto_increment primary key,user_name varchar(25),password varchar(25),gender varchar(25));
+insert into user_account values(null, 'Manoj', "javatest", "java");
